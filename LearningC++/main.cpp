3babc3086682800ddb9373c5940a2706ba42e93c
Edmond <34758460+EdmondtheGreat@users.noreply.github.com>
2022-07-20 20:27:53 +0800

    Initial Commit

---
+++ LearningC++/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LearningC++
+//
+//  Created by OAA on 20/07/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
